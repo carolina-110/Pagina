41657cb635337274862ef826df54148c475996d7
carolina-110 <carolina-110@hotmail.com>
2019-07-05 21:27:41 -0500

    Nuevo

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#mobile-demo').sidenav();
+  });
+
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+      
+  $(document).ready(function() {
+    M.updateTextFields();
+  });
+        
